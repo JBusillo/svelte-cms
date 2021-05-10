cf676900b643e70b7ea0230b38579bebfeb45799
Joe Busillo <jbusillo@yahoo.com>
2021-05-10 16:04:55 -0500

    first commit

---
+++ src/lib/environmentVariables.js
@@ -0,0 +1,2 @@
+export const envApiPath = import.meta.env.VITE_SVELTEKIT_API_PATH
+export const envFilePath = import.meta.env.VITE_SVELTEKIT_FILEPATH
---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import sveltePreprocess from 'svelte-preprocess';
+
+export default {
+	preprocess: sveltePreprocess({ postcss: true }),
+	kit: {
+		// By default, `npm run build` will create a standard Node app.
+		// You can create optimized builds for different platforms by
+		// specifying a different adapter
+		// adapter: node(),
+		// adapter: vercel(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+}
